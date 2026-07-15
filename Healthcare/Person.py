55ee926833ab7b2966abb00c4643e99631a2f82a
KaviniMuthamizhvalavan <kavini.m@prodapt.com>
2026-07-15 20:30:35 +0530

    Add Person class with initialization and methods

---
+++ Healthcare/Person.py
@@ -0,0 +1,12 @@
+# models/person.py
+class Person:
+    def __init__(self, id, name, age):
+        self.id = id
+        self.name = name
+        self.age = age
+
+    def display_info(self):
+        return f"ID: {self.id}, Name: {self.name}, Age: {self.age}"
+
+    def get_details(self):
+        return {"id": self.id, "name": self.name, "age": self.age}
